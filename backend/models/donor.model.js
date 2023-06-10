52a0730d200934d0d3f42964f0c7a392f338c614
garimachandna <gchandna4@gmail.com>
2023-06-10 23:05:03 +0530

    added register/login api

--- backend/models/donor.model.js
+++ backend/models/donor.model.js
@@ -1,20 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const donorSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  uid: { type: String, required: true },
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
   age: { type: Number, required: true },
   bloodType: { type: String, required: true },
-  contactInfo: {
-    phone: { type: String, required: true },
-    email: { type: String, required: true },
-  },
+  contact: { type: Number, required: true },
   address: { type: String, required: true },
-  donationHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Transaction' }],
-  availability: [{ type: String }],
-  healthInfo: { type: String },
-  consentInfo: { type: String },
+  zipcode: { type: Number, required: true },
+  // donationHistory: [
+  //   { type: mongoose.Schema.Types.ObjectId, ref: "Transaction" },
+  // ],
+  // availability: [{ type: Boolean }],
+  // healthInfo: { type: String },
+  // consentInfo: { type: String },
 });
 
-const Donor = mongoose.model('Donor', donorSchema);
+const Donor = mongoose.model("Donor", donorSchema);
 
 module.exports = Donor;
